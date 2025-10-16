ff68d89a2242bd423394d64181e3517e3e0567f4
Nahuel Dolores <contact@nahu.me>
2025-10-16 14:21:38 +0200

    Transformation support for display entities

--- core/src/main/java/io/th0rgal/oraxen/utils/VectorUtils.java
+++ core/src/main/java/io/th0rgal/oraxen/utils/VectorUtils.java
@@ -1,10 +1,12 @@
 package io.th0rgal.oraxen.utils;
 
 import org.bukkit.util.Vector;
+import org.joml.Vector3f;
 
 public class VectorUtils {
 
-    private VectorUtils() {}
+    private VectorUtils() {
+    }
 
     public static void rotateAroundAxisX(Vector v, double angle) {
         double cos = Math.cos(angle);
@@ -20,6 +22,22 @@ public class VectorUtils {
         double x = v.getX() * cos + v.getZ() * sin;
         double z = v.getX() * -sin + v.getZ() * cos;
         v.setX(x).setZ(z);
+    }
+
+    public static Vector3f parseVector3f(String input) {
+        if (input == null) {
+            return null;
+        }
+
+        final String[] parts = input.split(",");
+        if (parts.length != 3) {
+            return null;
+        }
+        try {
+            return new Vector3f(Float.parseFloat(parts[0]), Float.parseFloat(parts[1]), Float.parseFloat(parts[2]));
+        } catch (NumberFormatException ignored) {
+            return null;
+        }
     }
 
 }
